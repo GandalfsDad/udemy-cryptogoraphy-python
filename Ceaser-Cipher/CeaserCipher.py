6195ca463c7c1aceefe76d6f0b47bc436807e6a7
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-07-06 19:41:11 +1000

    Add Decryption function

--- Ceaser-Cipher/CeaserCipher.py
+++ Ceaser-Cipher/CeaserCipher.py
@@ -21,7 +21,7 @@ def generateKey(n : int) -> Dict[str,str]:
 
     return key
 
-def encrypt(key, msg):
+def encrypt(key : Dict[str, str], msg : str) -> str:
     """encrypt a message using a key
 
     Parameters
@@ -36,13 +36,36 @@ def encrypt(key, msg):
     str
         A encrypted message
     """
-    
+
     converted = [key[c] if c in key else c for c in msg]
     return ''.join(converted)
+
+def decrypt(key : Dict[str, str], msg : str) -> str:
+    """decrypt a message using a key
+
+    Parameters
+    ----------
+    key : Dict[str,str]
+        A dictionary cipher key
+    msg : str
+        A message to decrypt
+
+    Returns
+    -------
+    str
+        A decrypted message
+    """
+    reverseKey = {v: k for k, v in key.items()}
+
+    return encrypt(reverseKey, msg)
+
 
 if __name__ == "__main__":
     key = generateKey(3)
     msg = "Fred is a twat".upper()
+    encrypted = encrypt(key, msg)
+    decrypted = decrypt(key, encrypted)
 
     print(f"Unencrpyted: {msg}")
-    print(f"Encrypted: {encrypt(key, msg)}")
+    print(f"Encrypted: {encrypted}")
+    print(f"Decrypted: {decrypted}")
